426d7609a6310fd32cba7fc088b1405cc1f3dcae
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2024-09-02 20:20:26 +0530

    Change transmitter and add DVT
    Minor changes to i2c_transmitter.v
    Add DVT IDE for VS Code

--- imu_sensor_data_acquisition/imu_sensor_data_acquisition.srcs/sim_1/new/i2c_transmitter_tb.v
+++ imu_sensor_data_acquisition/imu_sensor_data_acquisition.srcs/sim_1/new/i2c_transmitter_tb.v
@@ -1,36 +1,37 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 09/02/2024 10:13:11 AM
-// Design Name: 
+// Design Name:
 // Module Name: i2c_transmitter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
 
 module i2c_transmitter_tb
-#(
-    parameter BUS_WIDTH = 8,
-    parameter DATA_WIDTH = 3
-);
+    #(
+        parameter BUS_WIDTH = 8,
+        parameter DATA_WIDTH = 3
+    );
 
     // Inputs
     reg clk_50m;
     reg [BUS_WIDTH - 1:0] data_in;
     reg is_addr;
+    reg read_write;
     reg send_en;
 
     // Outputs
@@ -39,7 +40,7 @@ module i2c_transmitter_tb
     wire is_busy;
 
     // Instantiate transmitter
-    transmitter
+    i2c_transmitter
     #(
         .BUS_WIDTH(BUS_WIDTH),
         .DATA_WIDTH(DATA_WIDTH)
@@ -49,6 +50,7 @@ module i2c_transmitter_tb
         .clk(clk_50m),
         .data_in(data_in),
         .is_addr(is_addr),
+        .read_write(read_write),
         .send_en(send_en),
         .sda(sda),
         .scl(scl),
@@ -66,6 +68,7 @@ module i2c_transmitter_tb
         #40;
         data_in = 8'h68;
         is_addr = 1'b1;
+        read_write = 1'b0;
 
         #40;
         send_en = 1'b1;
@@ -76,6 +79,7 @@ module i2c_transmitter_tb
         #1000;
         data_in = 8'h6B;
         is_addr = 1'b0;
+        read_write = 1'b0;
         send_en = 1'b1;
 
         #20;
--- imu_sensor_data_acquisition/imu_sensor_data_acquisition.srcs/sources_1/new/i2c_transmitter.v
+++ imu_sensor_data_acquisition/imu_sensor_data_acquisition.srcs/sources_1/new/i2c_transmitter.v
@@ -1,41 +1,42 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 09/02/2024 10:10:03 AM
-// Design Name: 
+// Design Name:
 // Module Name: i2c_transmitter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 module i2c_transmitter
-#(
-    parameter BUS_WIDTH = 8,
-    parameter DATA_WIDTH = 3
-)
-(
-    input wire clk,
-    input wire [BUS_WIDTH - 1:0] data_in,
-    input wire is_addr,
-    input wire send_en,
-    output reg sda,
-    output reg scl,
-    output reg is_busy
-);
+    #(
+        parameter BUS_WIDTH = 8,
+        parameter DATA_WIDTH = 3
+    )
+    (
+        input wire clk,
+        input wire [BUS_WIDTH - 1:0] data_in,
+        input wire is_addr,
+        input wire read_write,
+        input wire send_en,
+        output reg sda,
+        output reg scl,
+        output reg is_busy
+    );
 
     reg [BUS_WIDTH - 1:0] data;
-    reg [DATA_WIDTH:0] bits_sent;
+    reg [DATA_WIDTH:0] bits_sent = 0;
     reg [2:0] state;
     reg [1:0] bit_state;
 
@@ -51,154 +52,81 @@ module i2c_transmitter
     always @(posedge clk)
     begin
         if (send_en && state == IDLE)
+        begin
+            data <= data_in;
+            if (is_addr)
             begin
-                data <= data_in;
-                if (is_addr)
-                    begin
-                        bits_sent <= 4'b0001;
-                    end
-                else
-                    begin
-                        bits_sent <= 4'b0000;
-                    end
-                state = START;
+                if (read_write)
+                begin
+                    data[0] <= read_write;
+                end
             end
+            state <= START;
+        end
         else
-            begin
-                case (state)
-                    IDLE :
-                    begin
-                        is_busy = 1'b0;
-                        sda <= 1'bZ;
-                        scl <= 1'bZ;
-                    end
+        begin
+            case (state)
+                IDLE :
+                begin
+                    is_busy <= 1'b0;
+                    sda <= 1'bZ;
+                    scl <= 1'bZ;
+                end
 
-                    START :
+                START :
+                begin
+                    is_busy <= 1'b1;
+                    case (bit_state)
+                        BEFORE_CLK :
+                        begin
+                            sda <= 1'b0;
+                            scl <= 1'b1;
+                            bit_state <= AT_CLK;
+                        end
+                        AT_CLK :
+                        begin
+                            sda <= 1'b0;
+                            scl <= 1'b1;
+                            bit_state <= AFTER_CLK;
+                        end
+                        AFTER_CLK:
+                        begin
+                            sda <= 1'b0;
+                            scl <= 1'b0;
+                            bit_state <= BEFORE_CLK;
+                            state <= SENDING;
+                        end
+                        default :
+                        begin
+                            sda <= 1'bZ;
+                            scl <= 1'bZ;
+                        end
+                    endcase
+                end
+
+                SENDING :
+                begin
+                    is_busy <= 1'b1;
+
+                    if (bits_sent != BUS_WIDTH)
                     begin
-                        is_busy <= 1'b1;
+                        sda <= data[BUS_WIDTH - 1 - bits_sent];
                         case (bit_state)
                             BEFORE_CLK :
                             begin
-                                sda <= 1'b0;
-                                scl <= 1'b1;
-                                bit_state <= AT_CLK;
-                            end
-                            AT_CLK :
-                            begin
-                                sda <= 1'b0;
-                                scl <= 1'b1;
-                                bit_state <= AFTER_CLK;
-                            end
-                            AFTER_CLK:
-                            begin
-                                sda <= 1'b0;
                                 scl <= 1'b0;
-                                bit_state <= BEFORE_CLK;
-                                state <= SENDING;
-                            end
-                            default :
-                            begin
-                                sda <= 1'bZ;
-                                scl <= 1'bZ;
-                            end
-                        endcase
-                    end
-
-                    SENDING :
-                    begin
-                        is_busy <= 1'b1;
-
-                        if (bits_sent != BUS_WIDTH)
-                            begin
-                                sda <= data[BUS_WIDTH - 1 - bits_sent];
-                                case (bit_state)
-                                    BEFORE_CLK :
-                                    begin
-                                        scl <= 1'b0;
-                                        bit_state <= AT_CLK;
-                                    end
-                                    AT_CLK :
-                                    begin
-                                        scl <= 1'b1;
-                                        bit_state <= AFTER_CLK;
-                                    end
-                                    AFTER_CLK :
-                                    begin
-                                        scl <= 1'b0;
-                                        bit_state <= BEFORE_CLK;
-                                        bits_sent <= bits_sent + 4'b0001;
-                                    end
-                                    default :
-                                    begin
-                                        sda <= 1'bZ;
-                                        scl <= 1'bZ;
-                                    end
-                                endcase
-                            end
-                        else
-                            if (is_addr)
-                                begin
-                                    state <= WAIT;
-                                end
-                            else
-                                begin
-                                    state <= STOP;
-                                end
-
-                    end
-
-                    WAIT:
-                    begin
-                        case (bit_state)
-                            BEFORE_CLK :
-                            begin
-                                sda <= 1'bZ;
-                                scl <= 1'bZ;
                                 bit_state <= AT_CLK;
                             end
                             AT_CLK :
                             begin
-                                sda <= 1'bZ;
-                                scl <= 1'bZ;
+                                scl <= 1'b1;
                                 bit_state <= AFTER_CLK;
                             end
                             AFTER_CLK :
                             begin
-                                sda <= 1'bZ;
-                                scl <= 1'bZ;
-                                bit_state <= BEFORE_CLK;
-                                state <= STOP;
-                            end
-                            default :
-                            begin
-                                sda <= 1'bZ;
-                                scl <= 1'bZ;
-                            end
-                        endcase
-                    end
-
-                    STOP :
-                    begin
-                        is_busy <= 1'b1;
-                        case (bit_state)
-                            BEFORE_CLK :
-                            begin
-                                sda <= 1'b1;
                                 scl <= 1'b0;
-                                bit_state <= AT_CLK;
-                            end
-                            AT_CLK :
-                            begin
-                                sda <= 1'b1;
-                                scl <= 1'b1;
-                                bit_state <= AFTER_CLK;
-                            end
-                            AFTER_CLK:
-                            begin
-                                sda <= 1'b1;
-                                scl <= 1'b1;
                                 bit_state <= BEFORE_CLK;
-                                state <= IDLE;
+                                bits_sent <= bits_sent + 4'b0001;
                             end
                             default :
                             begin
@@ -207,15 +135,90 @@ module i2c_transmitter
                             end
                         endcase
                     end
+                    else
+                        // if (is_addr)
+                        // begin
+                        state <= WAIT;
+                    // end
+                    // else
+                    // begin
+                    //     state <= STOP;
+                    // end
 
-                    default :
-                    begin
-                        is_busy = 1'b0;
-                        sda <= 1'bZ;
-                        scl <= 1'bZ;
-                    end
-                endcase
-            end
+                end
+
+                WAIT:
+                begin
+                    case (bit_state)
+                        BEFORE_CLK :
+                        begin
+                            sda <= 1'bZ;
+                            scl <= 1'bZ;
+                            bit_state <= AT_CLK;
+                        end
+                        AT_CLK :
+                        begin
+                            // sda <= 1'bZ;
+                            // sda should be made inout and read whether the receiver has acknowledged.
+                            // If not acknowledged, go back to start and resend the same byte.
+                            scl <= 1'bZ;
+                            bit_state <= AFTER_CLK;
+                        end
+                        AFTER_CLK :
+                        begin
+                            sda <= 1'bZ;
+                            scl <= 1'bZ;
+                            bit_state <= BEFORE_CLK;
+                            state <= STOP;
+                        end
+                        default :
+                        begin
+                            sda <= 1'bZ;
+                            scl <= 1'bZ;
+                        end
+                    endcase
+                end
+
+                STOP :
+                begin
+                    is_busy <= 1'b1;
+                    case (bit_state)
+                        BEFORE_CLK :
+                        begin
+                            sda <= 1'b1;
+                            scl <= 1'b0;
+                            bit_state <= AT_CLK;
+                        end
+                        AT_CLK :
+                        begin
+                            sda <= 1'b1;
+                            scl <= 1'b1;
+                            bit_state <= AFTER_CLK;
+                        end
+                        AFTER_CLK:
+                        begin
+                            sda <= 1'b1;
+                            scl <= 1'b1;
+                            bit_state <= BEFORE_CLK;
+                            bits_sent <= 0;
+                            state <= IDLE;
+                        end
+                        default :
+                        begin
+                            sda <= 1'bZ;
+                            scl <= 1'bZ;
+                        end
+                    endcase
+                end
+
+                default :
+                begin
+                    is_busy <= 1'b0;
+                    sda <= 1'bZ;
+                    scl <= 1'bZ;
+                end
+            endcase
+        end
     end
 
 
